85beff40634f2b3b2932fdc63301dce4f6a6f44f
xiuwen <smuis480@gmail.com>
2018-11-17 22:50:32 +0800

    removing unused comments

--- EpicFYPApp/src/java/Controller/updateUserServletLastName.java
+++ EpicFYPApp/src/java/Controller/updateUserServletLastName.java
@@ -37,37 +37,8 @@ public class updateUserServletLastName extends HttpServlet {
         // retrieve user input
         
         String userEmail = request.getParameter("userEmail");
-       // String userFirstName = request.getParameter("firstName");
         String userLastName = request.getParameter("lastName");
-//        String phone = request.getParameter("phone");
-//        int userPhone = Integer.parseInt(phone);
-//        String userGender = request.getParameter("gender");
-//        String userCitizenship = request.getParameter("citizenship");
-//        String yob = request.getParameter("yob");
-//        int yearOfBirth = Integer.parseInt(yob);
-//        String[] userInterestString = request.getParameterValues("interest");
-//        String userInterest = "";
-//        for (int i = 0; i < userInterestString.length; i++) {
-//            userInterest += "" + userInterestString[i];
-//            if (i != userInterestString.length - 1) {
-//                userInterest += ", ";
-//            }
-//        }
-        
-        //String userPassword = request.getParameter("password");
-//        String userOccupation = request.getParameter("occupation");
-//        
-//        String userHighestEducation = request.getParameter("highest_qualification");
-//        
-//        String userFieldOfStudy = request.getParameter("fos");
-//        String userDescription = request.getParameter("description");
-//        String userSchool = request.getParameter("school");
 
-        //if (!userFirstName.equals("") && !userLastName.equals("") && !userCitizenship.equals("") && !userHighestEducation.equals("") && !userSchool.equals("") && !userFieldOfStudy.equals("")) {
-
-            // Insert into database
-            //boolean insertedUser = UserDAO.updateUser(userEmail, userFirstName, userLastName, userPhone, userGender, userCitizenship, yearOfBirth, userInterest, userPassword, userOccupation, userHighestEducation, userFieldOfStudy, userDescription, userSchool);
-            //boolean insertedUser  = UserDAO.updateUserPassword(userEmail, userPassword);
             boolean insertedUser  = UserDAO.updateUserLastName(userEmail, userLastName);
             if (insertedUser == true) {
                 User user = UserDAO.getUserByID(userEmail);
